02ffbcca732d92477b977163ebf71b629d692a45
David <dabm@stanford.edu>
2021-07-12 11:06:35 -0700

    Second amendment of date.py

---
+++ main.py
@@ -0,0 +1,10 @@
+
+import david_test as dt
+
+my_name = dt.name_class()
+my_date = dt.date_class()
+print(f"Name: {my_name.name}")
+print(f"Date: {my_date.date}")
+print(f"That is all.")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
